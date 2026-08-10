3fcdb6dd599fbf6d4ede8d77e9bbb9bc0cb3c2e2
Mark55X <marco2002stefani@gmail.com>
2026-08-10 11:46:24 +0200

    compatibiity with RLBench

--- rlbench/tasks/reach_target_moving_cube.py
+++ rlbench/tasks/reach_target_moving_cube.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 import numpy as np
 from pyrep.objects.shape import Shape
 from pyrep.objects.proximity_sensor import ProximitySensor
@@ -17,11 +17,9 @@ class ReachTargetMovingCube(Task):
         self.cube = Shape('moving_cube')
 
     def init_episode(self, index: int) -> List[str]:
-        # self.cube.set_position([0.2, -0.2, 0.8])
-        
         # Constant velocity (10 cm/s)
         self.velocity = np.array([0.0, 0.1, 0.0])
-        
+
         return ['reach the red target while the cube moves',
                 'touch the red sphere and ignore the moving block']
 
@@ -32,3 +30,9 @@ class ReachTargetMovingCube(Task):
         current_pos = self.cube.get_position()
         new_pos = current_pos + self.velocity * 0.05
         self.cube.set_position(new_pos)
+
+    def base_rotation_bounds(self) -> Tuple[List[float], List[float]]:
+        return [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]
+
+    def is_static_workspace(self) -> bool:
+        return True
